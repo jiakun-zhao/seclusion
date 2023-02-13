db9119953f8a3874d7af752074db0d6aa9fafad8
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-02-14 04:58:08 +0800

    fix: guo route err

---
+++ src/pages/guo/[id].vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import raw from '~/assets/guo.images.json'
+const { id } = defineProps<{ id: string }>()
+const imgs = raw[Number(id)].images.map(i => i.src)
+</script>
+
+<template>
+    <div class="guo">
+        <h2>作品集.</h2>
+        <RouterLink to="/guo">返回</RouterLink>
+        <div class="grid">
+            <a v-for="(i, idx) of imgs" :key="idx" href="javascript:void(0)">
+                <img :src="i" alt="Image" />
+            </a>
+        </div>
+    </div>
+</template>
---
+++ src/pages/guo/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import raw from '~/assets/guo.images.json'
+const imgs = raw.map(i => i.images[0].src)
+</script>
+
+<template>
+    <div class="guo">
+        <h2>作品集.</h2>
+        <RouterLink to="/">首页</RouterLink>
+        <div class="grid">
+            <RouterLink v-for="(i, id) of imgs" :key="id" :to="`/guo/${id}`">
+                <img :src="i" alt="Image" />
+            </RouterLink>
+        </div>
+    </div>
+</template>
